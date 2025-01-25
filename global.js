dfa31a9135264d6c5028045fd86a9a9a4d386125
AdamCCross <accross2003@icloud.com>
2025-01-24 17:39:12 -0800

    Create nav with js

--- global.js
+++ global.js
@@ -3,3 +3,41 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+//// Create Naviation in JS ////
+
+// create pages
+let pages = [
+    { url: '', title: 'Home'},
+    { url: 'projects/', title: 'Projects'},
+    { url: 'resume/', title: 'Resume'},
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/AdamCCross', title: 'Github'},
+]
+
+// add nav element to the body of page
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+// create constant variable that checks for home class in page
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+// create links and add attributes as needed
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    // create link and add it to nav
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    // add current class to current page
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    // have link open new path if external link
+    if (a.host !== location.host) {
+        a.target = "_blank";
+      }
+}
