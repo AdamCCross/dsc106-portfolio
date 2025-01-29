15c1aa2f19393b529e45a2c7a245169ebdee31b2
AdamCCross <accross2003@icloud.com>
2025-01-29 15:13:35 -0800

    debugging latest projects on home page

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { fetchJSON, renderProjects} from '../global.js';
+import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 
+console.log('Projects:', latestProjects);
 renderProjects(latestProjects, projectsContainer, 'h2');
